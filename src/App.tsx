3d863bfdd36d2db386d096dfc27b36306c3ae155
NalinPriyankara <nalinpriyankara033@gmail.com>
2025-09-01 15:35:51 +0530

    start

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import AboutUs from "./pages/AboutUs"
+
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline">
+        Hello world!
+      <AboutUs />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AboutUs:React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        About Us Page
+      </h1>
+      <p className="mt-4 text-lg">
+        Welcome to the About Us page. We are dedicated to providing the best service possible.
+      </p>
+    </div>
+  )
+}
+
+export default AboutUs
