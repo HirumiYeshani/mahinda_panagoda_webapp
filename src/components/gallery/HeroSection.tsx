7f497ef09f89bcbee94cfc99bb9dc5170816b9be
Yeshani <2005yesha@gmail.com>
2025-09-22 10:26:02 +0530

    Updated

--- src/components/gallery/HeroSection.tsx
+++ src/components/gallery/HeroSection.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import HeroImg from "../../assets/img/gallery/Frame 1.svg";
-
+import Navbar from "../../assets/img/gallery/nav.svg";
 const HeroSection: React.FC = () => {
   return (
     <div>
       <div className="relative  h-[300px]  lg:h-[500px]">
+                <div className="absolute top-0 left-0 w-full">
+          <img src ={Navbar} className="w-full"/>
+</div>
         <img
           src={HeroImg}
           alt="Contact Hero"
@@ -14,7 +17,7 @@ const HeroSection: React.FC = () => {
           <div className="w-11/12 md:w-5/6 mx-auto py-4 space-y-2">
             <h1 className="text-2xl lg:text-5xl leading-[34px] lg:leading-[62px] font-bold font-belda text-white">
               Capturing Moments In{" "}
-              <span className="block sm:inline text-primary">
+              <span className=" text-primary">
                 Images & Motion
               </span>
             </h1>
