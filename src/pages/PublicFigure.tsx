66fbe158164257bcb34a9926af9f5dc95969fa13
Yeshani <2005yesha@gmail.com>
2025-09-30 21:57:27 +0530

    Updated the Public Figures Page

--- src/pages/PublicFigure.tsx
+++ src/pages/PublicFigure.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import HeroSection from '../components/publicfigures/HeroSection'
-import PublicFigures from '../components/publicfigures/CardSection'
+import React from "react";
+import HeroSection from "../components/publicfigures/HeroSection";
+import PublicFigures from "../components/publicfigures/CardSection";
+import Navbar from "../components/common/Navbar";
+import Footer from "../components/common/Footer";
 
-
-const PublicFigure:React.FC = () => {
+const PublicFigure: React.FC = () => {
   return (
-      <div className="font-[roboto]">
+    <div className="font-[roboto]">
+      <section className="w-full">
+        <Navbar/>
+      </section>
       <section className="w-full lg:mb-30 mb-20">
-        <HeroSection/>
+        <HeroSection />
       </section>
 
       <section className="md:w-5/6 mx-auto w-11/12 lg:mb-30 mb-20 ">
-        <PublicFigures/>
+        <PublicFigures />
       </section>
 
-       
+      <section className="w-full">
+        <Footer/>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default PublicFigure
+export default PublicFigure;
